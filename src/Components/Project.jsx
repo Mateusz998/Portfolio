ba3bbfb553c88e50dc0f6457129ef646bbadb4f4
Mateusz998 <kurowskimateusz32@gmail.com>
2023-06-10 12:08:53 +0200

    fixed projects bug

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 
-// .child-button:hover .parent-imf {
-	// background: #000000;
-	// 	opacity: 0.55;
-// }
 export default function Project(props) {
 	return (
 		<div>
 			<div className='project--container'>
-				<img src={[props.sideSS]} alt='' className='webpage' />
-				<div className='project--img--button'>
-					<button className='hover--button'>
-						<a href={props.webside}>VIEW PROJECT</a>
-					</button>
-					<button className='hover--button'>
-						<a href={props.code}>VIEW CODE</a>
-					</button>
+				<div className='screenshot'>
+					<img src={[props.sideSS]} alt='' className='webpage' />
+					<div className='project--img--button'>
+						<button className='hover--button'>
+							<a href={props.webside}>VIEW PROJECT</a>
+						</button>
+						<button className='hover--button'>
+							<a href={props.code}>VIEW CODE</a>
+						</button>
+					</div>
 				</div>
 			</div>
 			<div className='project--bottom'>
